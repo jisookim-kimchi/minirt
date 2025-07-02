5ae0aaf5491609787e57cd6f8ff48b8060a9b4bc
Jisoo Kim <jisokim2@3-C-2.42heilbronn.de>
2025-07-02 16:19:10 +0200

    okay

---
+++ rgb_print.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include "struct.h"
+
+
+void    write_color(t_color3 pixel_color)
+{
+    printf("%d %d %d\n", (int)(255.999 * pixel_color.x),
+                        (int)(255.999 * pixel_color.y),
+                        (int)(255.999 * pixel_color.z));
+}
+
+int	main(void)
+{
+    int     i;
+    int     j;
+    double  r;
+    double  g;
+    double  b;
+    int     canvas_width;
+    int     canvas_height;
+
+    //캔버스의 가로, 세로 픽셀값
+    canvas_width = 256;
+    canvas_height = 256;
+
+    // 랜더링
+    // P3 는 색상값이 아스키코드라는 뜻, 그리고 다음 줄은 캔버스의 가로, 세로 픽셀 수, 마지막은 사용할 색상값
+    printf("P3\n%d %d\n255\n", canvas_width, canvas_height);
+    j = canvas_height - 1;
+    while (j >= 0)
+    {
+        i = 0;
+        while (i < canvas_width)
+        {
+            r = (double)i / (canvas_width - 1);
+            g = (double)j / (canvas_height - 1);
+            b = 0.25;
+            printf("%d %d %d\n", (int)(255.999 * r), (int)(255.999 * g), (int)(255.999 * b));
+        	++i;
+        }
+    	--j;
+    }
+    return (0);
+}
