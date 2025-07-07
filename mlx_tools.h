dfd8948173d700ab19200d8a3dd9ddc1bdea976f
Tamas Farkas <tfarkas@2-C-3.42heilbronn.de>
2025-07-07 20:46:58 +0200

    Start to create window for practice. It has still problem during the compilation.

--- mlx_tools.h
+++ mlx_tools.h
@@ -6,13 +6,23 @@
 /*   By: tfarkas <tfarkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 18:43:53 by tfarkas           #+#    #+#             */
-/*   Updated: 2025/07/07 18:52:19 by tfarkas          ###   ########.fr       */
+/*   Updated: 2025/07/07 19:41:28 by tfarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MLX_TOOLS_H
 # define MLX_TOOLS_H
 
-# include "MLX42/include/MLX42/MLX42.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+# include <stdbool.h>
+
+# define IMAGE_WIDTH 1200
+# define IMAGE_RATIO 16/9
+
+typedef struct s_window
+{
+	mlx_t		*mlx;
+	mlx_image_t	*image;
+}	t_window;
 
 #endif
