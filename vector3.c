5575bc568e926aa11e204c179aba806cbf47b56f
Tamas Farkas <tfarkas@2-C-3.42heilbronn.de>
2025-07-09 19:43:51 +0200

    Copied the following files from jisoo to tamas: - math/trace.c - math/vector2.c - math/vector3.c - math/vector4.c - math/vector_struct.h - object/plane.h

---
+++ vector3.c
@@ -0,0 +1,161 @@
+#include "vector_struct.h"
+
+t_vec3  vec3_one()
+{
+    return (t_vec3){1.f, 1.f, 1.f};
+}
+
+t_vec3	vec3(double x, double y, double z)
+{
+    t_vec3	vec;
+
+    vec.x = x;
+    vec.y = y;
+    vec.z = z;
+    
+	return (vec);
+}
+
+t_point3	point3(double x, double y, double z)
+{
+	t_point3	point;
+	
+	point.x = x;
+	point.y = y;
+	point.z = z;
+	
+	return (point);
+}
+
+t_color3	color3(double x, double y, double z)
+{
+	t_color3	color3;
+
+	color3.x = x;
+	color3.y = y;
+	color3.z = z;
+
+	return (color3);
+}
+
+void	vec3_set(t_vec3 *vec3, double x, double y, double z)
+{
+	vec3->x = x;
+	vec3->y = y;
+	vec3->z = z;
+}
+
+void	color_set(t_color3 *color3, double x, double y, double z)
+{
+	color3->x = x;
+	color3->y = y;
+	color3->z = z;
+}
+
+void	point_set(t_point3 *point3, double x, double y, double z)
+{
+	point3->x = x;
+	point3->y = y;
+	point3->z = z;
+}
+
+//vector len squared
+double	vec3_length_squared(t_vec3 vec)
+{
+    return (vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
+
+//vector length
+double      vec3_length(t_vec3 vec)
+{
+    return (sqrt(vec3_length_squared(vec)));
+}
+
+t_vec3	vec3_add(t_vec3 vec, double x, double y, double z)
+{
+    vec.x += x;
+    vec.y += y;
+    vec.z += z;
+    return (vec);
+}
+
+t_vec3	vec3_plus_vec3(t_vec3 vec1, t_vec3 vec2)
+{
+    vec1.x += vec2.x;
+    vec1.y += vec2.y;
+    vec1.z += vec2.z;
+    return (vec1);
+}
+
+t_vec3      vec3_sub(t_vec3 vec, double x, double y, double z)
+{
+    vec.x -= x;
+    vec.y -= y;
+    vec.z -= z;
+    return (vec);
+}
+
+t_vec3      vec3_sub_vec3(t_vec3 vec1, t_vec3 vec2)
+{
+    vec1.x -= vec2.x;
+    vec1.y -= vec2.y;
+    vec1.z -= vec2.z;
+    return (vec1);
+}
+
+t_vec3      vec3_multiply(t_vec3 vec, double t)
+{
+    vec.x *= t;
+    vec.y *= t;
+    vec.z *= t;
+    return (vec);
+}
+
+t_vec3      vec3_multiply_vec3(t_vec3 vec1, t_vec3 vec2)
+{
+    vec1.x *= vec2.x;
+    vec1.y *= vec2.y;
+    vec1.z *= vec2.z;
+    return (vec1);
+}
+
+t_vec3      vec3_divide(t_vec3 vec, double t)
+{
+    vec.x *= 1 / t;
+    vec.y *= 1 / t;
+    vec.z *= 1 / t;
+
+    return vec;
+}
+
+//vector dot
+double	vec3_dot(t_vec3 vec1, t_vec3 vec2)
+{
+    return (vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z);
+}
+
+//vector cross
+t_vec3	vec3_cross(t_vec3 vec1, t_vec3 vec2)
+{
+    t_vec3 new;
+
+    new.x = vec1.y * vec2.z - vec1.z * vec2.y;
+    new.y = vec1.z * vec2.x - vec1.x * vec2.z;
+    new.z = vec1.x * vec2.y - vec1.y * vec2.x;
+    return (new);
+}
+
+//get unit vector
+t_vec3	vec3_normalized(t_vec3 vec3)
+{
+	double len = vec3_length(vec3);
+	if (len == 0.0)
+	{
+		printf("vec2_normalized: length is 0, cannot normalize\n");
+		exit(0);
+	}
+	vec3.x = vec3.x / len;
+	vec3.y = vec3.y / len;
+	vec3.z = vec3.z / len;
+	return vec3;
+}
