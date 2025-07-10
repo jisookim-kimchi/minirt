58e3871aa6654ee02f7a451d172f666f8ac03555
Tamas Farkas <tfarkas@2-C-3.42heilbronn.de>
2025-07-10 14:54:47 +0200

    Copied the following files from jisoo to tamas: - math/screenpoint.c

---
+++ math/screenpoint.c
@@ -0,0 +1,22 @@
+#include "mathheader.h"
+
+/*
+    X * Y = pixelcounts on screen
+*/
+
+t_screenpoint make_screen(int in_x, int in_y)
+{
+    t_screenpoint screen;
+
+    screen.x = in_x;
+    screen.y = in_y;
+    screen.aspect_ratio = screen.x / screen.y;
+
+    return (screen);
+}
+
+float   get_screen_aspect_ratio(t_screenpoint *screen)
+{
+    return (screen->aspect_ratio);
+}
+
